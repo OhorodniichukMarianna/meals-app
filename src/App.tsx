5040425f6ed318eb430b536bff363172cadf3fa0
OhorodniichukMarianna <m.ohorodniichuk@gmail.com>
2025-03-03 23:14:30 +0200

    fix router

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => {
   return (
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
-        <Router>
+        <Router basename="/meals-app">
           <Routes>
             <Route path="/" element={<MealsPage />} />
             <Route path="/favourites" element={<FavouritesPage />} />
